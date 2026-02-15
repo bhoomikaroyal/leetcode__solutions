6924370982b51c5d5dff97e012c776c9bd459975
bhoomikaroyal <bhoomikaroyal1239@gmail.com>
2026-02-15 14:45:01 +0530

    Time: 31 ms (56.61%) | Memory: 54.3 MB (75.11%) - LeetSync

---
+++ 937-online-stock-span/online-stock-span.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+class StockSpanner {
+
+    // Pair: {price, span}
+    private Stack<int[]> stack;
+
+    public StockSpanner() {
+        stack = new Stack<>();
+    }
+
+    public int next(int price) {
+
+        int span = 1;
+
+        // Merge previous smaller prices
+        while (!stack.isEmpty() && stack.peek()[0] <= price) {
+            span += stack.pop()[1];
+        }
+
+        // Push current price and span
+        stack.push(new int[]{price, span});
+
+        return span;
+    }
+}
