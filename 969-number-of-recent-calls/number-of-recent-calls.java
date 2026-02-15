8abbbb685ce3274f181dbd79c32435c0f80643d7
bhoomikaroyal <bhoomikaroyal1239@gmail.com>
2026-02-15 15:37:12 +0530

    Time: 23 ms (31.58%) | Memory: 59.8 MB (34.50%) - LeetSync

---
+++ 969-number-of-recent-calls/number-of-recent-calls.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+class RecentCounter {
+
+    private Queue<Integer> queue;
+
+    public RecentCounter() {
+        queue = new LinkedList<>();
+    }
+
+    public int ping(int t) {
+
+        queue.offer(t);
+
+        // Remove old requests
+        while (!queue.isEmpty() && queue.peek() < t - 3000) {
+            queue.poll();
+        }
+
+        return queue.size();
+    }
+}
+
+
+/**
+ * Your RecentCounter object will be instantiated and called as such:
+ * RecentCounter obj = new RecentCounter();
+ * int param_1 = obj.ping(t);
+ */
