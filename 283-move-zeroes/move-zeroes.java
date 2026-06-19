35d2c6fdc73bb29baec8e7da2b211c1f230209bf
Vuppula Bhoomika <bhoomikaroyal1239@gmail.com>
2026-06-19 21:58:33 +0530

    Time: 2 ms (91.94%) | Memory: 47.9 MB (19.63%) - LeetSync

--- 283-move-zeroes/move-zeroes.java
+++ 283-move-zeroes/move-zeroes.java
@@ -1,17 +1,14 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        int idx = 0;
-
-        // Move all non-zero elements forward
-        for (int num : nums) {
-            if (num != 0) {
-                nums[idx++] = num;
+        int n = nums.length;
+        int i =0;
+        for(int j =0;j<n;j++){
+            if(nums[j] != 0){
+                int temp = nums[j];
+                nums[j]=nums[i];
+                nums[i] = temp;
+                i++;
             }
-        }
-
-        // Fill the rest with zeros
-        while (idx < nums.length) {
-            nums[idx++] = 0;
         }
     }
 }
